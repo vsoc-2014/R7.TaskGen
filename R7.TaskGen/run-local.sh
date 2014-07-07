078d04ae0052e1b85603d0fb6ac5900c399cad5e
Spiridonov1 <magnitslik@gmail.com>
2014-07-07 11:12:05 +0400

    Add install, run, uninstall scripts

---
+++ R7.TaskGen/run-local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Usage:
+
+# ./run-local.sh
+
+PROJECTNAME="R7.TaskGen"
+
+cd $(dirname $0)
+
+exec mono "./${PROJECTNAME}.exe" "$@"
---
+++ R7.TaskGen/uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Usage:
+
+# sudo ./uninstall.sh
+
+PREFIX="/usr/local"
+
+PROJECTNAME="R7.TaskGen"
+
+PROJECTNAME_LC="${PROJECTNAME,,}"
+
+# remove desktop file
+
+rm -f "${PREFIX}/share/applications/${PROJECTNAME_LC}.desktop" 
+
+# remove simlink to start script
+
+rm -f "${PREFIX}/bin/${PROJECTNAME_LC}"
+
+# remove application directory and all files
+
+rm -f -r "${PREFIX}/lib/${PROJECTNAME_LC}"
+
+echo "Uninstall of ${PROJECTNAME} complete."
