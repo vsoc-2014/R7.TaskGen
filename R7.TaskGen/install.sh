5b256fbe1365a2aa29ffdb9b045183de18c470f7
vladimirefimov <volodya_volgograd@mail.ru>
2014-07-10 00:43:46 +0400

    Add icon "r7.taskgen.svg"

--- R7.TaskGen/install.sh
+++ R7.TaskGen/install.sh
@@ -16,22 +16,28 @@ cd $(dirname ${0})
 
 # make some dirs, if they does't where
 
-mkdir -p "${PREFIX}/lib/${PROJECTNAME_LC}"
+sudo mkdir -p "${PREFIX}/lib/${PROJECTNAME_LC}"
 
-mkdir -p "${PREFIX}/share/applications"
+sudo mkdir -p "${PREFIX}/share/applications"
 
 mkdir -p "${HOME}/.config/${PROJECTNAME_LC}"
 
+cp -r -f "./App_Data/taskgen.sqlite" "${HOME}/.config/${PROJECTNAME_LC}"
+
 # install project to application directory
 
-cp -r -f "." "${PREFIX}/lib/${PROJECTNAME_LC}"
+sudo cp -r -f "." "${PREFIX}/lib/${PROJECTNAME_LC}"
+
+cp -r -f "./${PROJECTNAME}.exe.config" "${HOME}/.config/${PROJECTNAME_LC}/user.config"
+
+sudo cp -r -f "./${PROJECTNAME_LC}.svg" "/usr/share/pixmaps"
 
 # make symlink to start script in prefix/bin
 
-ln -s "${PREFIX}/lib/${PROJECTNAME_LC}/run.sh" "${PREFIX}/bin/${PROJECTNAME_LC}"
+sudo ln -s "${PREFIX}/lib/${PROJECTNAME_LC}/run.sh" "${PREFIX}/bin/${PROJECTNAME_LC}"
 
 # install desktop file
 
-cp -r -f "${PROJECTNAME_LC}.desktop" "${PREFIX}/share/applications"
+sudo cp -r -f "${PROJECTNAME_LC}.desktop" "${PREFIX}/share/applications"
 
 echo "Installation of ${PROJECTNAME} complete."
